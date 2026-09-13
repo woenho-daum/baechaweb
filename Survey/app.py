2e5d5e9a01097e789e2ef8c76cec839d270adfa8
woenho-daum <woenho@daum.net>
2026-09-13 13:10:56 +0900

    개발환경 구축

--- Survey/app.py
+++ Survey/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, abort, request  # <-- 기존 Flask 임포트에 request, abort 추가
 
 app = Flask(__name__)
 
+# --- [추가] 불필요한 스캔 경로 차단 로직 ---
+BLOCKED_PATHS = ['.git', '.env', 'config.json', 'config.js', 'env.js','robots.txt' ]
 
+@app.before_request
+def block_scanners():
+    path = request.path.lower()
+    if any(bad in path for bad in BLOCKED_PATHS) or '.git' in path:
+        abort(403)
+# ---------------------------------------------
+    
 @app.route("/")
 def index():
     return "Survey App is running!"
